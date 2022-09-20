b1b20b870a6ecd368b70442c9fbebd44dccb8e8a
NodirGH <mahkamov.nodirbek.95@gmail.com>
2022-09-20 22:25:39 +0500

    Part1Question UI changed

--- app/src/main/java/com/example/ieltsspeaking/home/category/part1Topic/adapter/Part1TopicAdapter.kt
+++ app/src/main/java/com/example/ieltsspeaking/home/category/part1Topic/adapter/Part1TopicAdapter.kt
@@ -5,6 +5,8 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ieltsspeaking.databinding.ItemsRvPart1TopicsBinding
 import com.example.ieltsspeaking.home.category.part1Topic.model.ModelPart1Topic
+import com.example.ieltsspeaking.home.category.part1Topic.questions.data.Part1QuestionsStudyData
+import com.example.ieltsspeaking.home.category.part1Topic.questions.model.ModelPart1Questions
 
 class Part1TopicAdapter(
     private val part1ModelTopic: List<ModelPart1Topic>,
@@ -27,12 +29,12 @@ class Part1TopicAdapter(
 
     override fun getItemCount() = part1ModelTopic.size
 
-    inner class Part1ViewHolder(private val binding: ItemsRvPart1TopicsBinding) :
+    inner class Part1ViewHolder(val binding: ItemsRvPart1TopicsBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
         fun bindView(modelPart1Topic: ModelPart1Topic) {
             binding.tvPart1OrderNumber.text = modelPart1Topic.orderNumber.toString()
-            binding.tvPart1NumberQuestion.text = modelPart1Topic.questionCount.toString()
+            binding.tvPart1NumberQuestion.text = part1ModelTopic.size.toString()
             binding.tvPart1TopicHeading.text = modelPart1Topic.heading
             binding.ivPart1BgRectangle.setImageResource(modelPart1Topic.background)
         }
