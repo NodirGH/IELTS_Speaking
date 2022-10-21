bba1f355a7cb3f981625fe93875d307fc7bf8263
NodirGH <mahkamov.nodirbek.95@gmail.com>
2022-10-21 22:40:06 +0500

    part 3 fragment is ready (fully populated)

--- app/src/main/java/com/example/ieltsspeaking/home/category/part3Topic/data/Part3TopicData.kt
+++ app/src/main/java/com/example/ieltsspeaking/home/category/part3Topic/data/Part3TopicData.kt
@@ -7,16 +7,16 @@ class Part3TopicData {
     companion object{
         fun getPart3TopicData(): List<ModelPartsTopic>{
             return listOf(
-                ModelPartsTopic(1, 1, "Weather", 15, R.drawable.bg_part1_5),
+                ModelPartsTopic(1, 1, "Weather", 8, R.drawable.bg_part1_5),
                 ModelPartsTopic(2, 2, "Travel & Tourism", 15, R.drawable.bg_part1_6),
-                ModelPartsTopic(3, 3, "Technology & Devices", 15, R.drawable.bg_part1_7),
-                ModelPartsTopic(4, 4, "Sleep & Being late", 15, R.drawable.bg_part1_8),
-                ModelPartsTopic(5, 5, "Shopping", 15, R.drawable.bg_part1_9),
-                ModelPartsTopic(6, 6, "Service", 15, R.drawable.bg_part1_10),
+                ModelPartsTopic(3, 3, "Technology & Devices", 9, R.drawable.bg_part1_7),
+                ModelPartsTopic(4, 4, "Sleep & Being late", 12, R.drawable.bg_part1_8),
+                ModelPartsTopic(5, 5, "Shopping", 12, R.drawable.bg_part1_9),
+                ModelPartsTopic(6, 6, "Service", 6, R.drawable.bg_part1_10),
                 ModelPartsTopic(7, 7, "Pollution", 15, R.drawable.bg_part1_1),
-                ModelPartsTopic(8, 8, "Politeness", 15, R.drawable.bg_part1_2),
-                ModelPartsTopic(9, 9, "Plans", 15, R.drawable.bg_part1_3),
-                ModelPartsTopic(10, 10, "People, Family & Friend", 15, R.drawable.bg_part1_4),
+                ModelPartsTopic(8, 8, "Politeness", 9, R.drawable.bg_part1_2),
+                ModelPartsTopic(9, 9, "Plans", 8, R.drawable.bg_part1_3),
+                ModelPartsTopic(10, 10, "People, Family & Friend", 28, R.drawable.bg_part1_4),
             )
         }
     }
