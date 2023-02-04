b52ecc73a458960938f8e9a0fbc02005f594735d
NodirGH <mahkamov.nodirbek.95@gmail.com>
2023-02-04 16:55:07 +0500

    mipmap is corrected

--- app/src/main/java/my/application/ieltsspeaking/home/drawer_layout/contact/ContactUsFragment.kt
+++ app/src/main/java/my/application/ieltsspeaking/home/drawer_layout/contact/ContactUsFragment.kt
@@ -1,5 +1,7 @@
 package my.application.ieltsspeaking.home.drawer_layout.contact
 
+import android.os.Bundle
+import android.view.View
 import my.application.ieltsspeaking.databinding.FragmentContactUsBinding
 import my.application.ieltsspeaking.home.BaseFragment
 
